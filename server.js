d0f58d8999e41df8cfe44851c249f65ac1e5cdb6
Craig Banach <craigsb93@gmail.com>
2016-04-01 14:47:46 +0000

    finished project

--- server.js
+++ server.js
@@ -1,31 +1,88 @@
 'use strict';
 
+require('dotenv').config();
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongoose = require('mongoose');
-var passport = require('passport');
-var session = require('express-session');
+var Search = require("bing.search");
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
-require('dotenv').load();
-require('./app/config/passport')(passport);
+var search = new Search(process.env.BING_KEY);
 
-mongoose.connect(process.env.MONGO_URI);
+app.get("/*", function(request, response) {
+	
+	var queryString = request.params[0];
+	
+	if (queryString === "") {
+		
+		// display most recent searches
+		MongoClient.connect("mongodb://localhost:27017/image_search", function(err, db) {
+			
+			if (err) throw err;
+			
+			var collection = db.collection("searches");
+			collection.find().sort({_id: -1}).limit(10).toArray( function(err, docs) {
+				if (err) throw err;
+				response.end(JSON.stringify(docs));
+			})
+		});
+	} else {
+		
+		var offset = 0;
+		
+		if (Object.keys(request.query).length !== 0) {
+			offset = request.query["offset"];
+		}
+		
+		bingSearch(queryString, Number(offset)).then(function(results) {
+			
+			// add results to db
+			MongoClient.connect("mongodb://localhost:27017/image_search", function(err, db) {
+				
+				if (err) throw err;
+				
+				let search = {
+					term: queryString,
+					when: new Date().toDateString()
+				}
+				console.log(search);
+				var collection = db.collection("searches");
+				collection.insert(search);
+				db.close();
+			});
+			
+			let output = results.map(function(result) {
+				return {
+					URL: result.url,
+					alt_text: result.title,
+					page_url: result.sourceUrl
+				};
+			});
+			console.log(output);
+			response.end(JSON.stringify(output));
+		});
+	}
 
-app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
+});
+function bingSearch(query, offset) {
 
-app.use(session({
-	secret: 'secretClementine',
-	resave: false,
-	saveUninitialized: true
-}));
+	var options = {
+		top: 10,
+		skip: offset * 10
+	};
 
-app.use(passport.initialize());
-app.use(passport.session());
+	return new Promise(function (resolve, reject) {
 
-routes(app, passport);
+		search.images(query, options, function (err, results) {
+			
+			if (err) {
+				console.log(err);
+				reject(err);
+			} else {
+				resolve(results);
+			}
+		});
+	});
+}
 
 var port = process.env.PORT || 8080;
 app.listen(port,  function () {
